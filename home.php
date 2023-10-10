c5b2f577a955cb8623799bba2f6385a77d17f61c
zoomero8 <d.r.iarovoi@stud.mospolytech.ru>
2023-10-10 22:49:43 +0300

    dwdw

--- home.php
+++ home.php
@@ -5,7 +5,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" type="text/css" href="style_answer.css">
+    <link rel="stylesheet" type="text/css" href="CSS/style_answer.css">
     <title>Ответ на обращение</title>
     
 </head>
