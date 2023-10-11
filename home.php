c2b5584c08bd08d890321888b5d391bb30ee0643
zoomero8 <d.r.iarovoi@stud.mospolytech.ru>
2023-10-11 15:23:10 +0300

    last update

--- home.php
+++ home.php
@@ -1,31 +1,29 @@
-<?php session_start();?>
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" type="text/css" href="CSS/style_answer.css">
-    <title>Ответ на обращение</title>
-    
-</head>
+<?php
+include 'header.html';
+?>
+<link rel="stylesheet" href="CSS/style_answer.css">
 <body>
     <?php
-    
-        $fio = isset($_GET['fio']) ? $_GET['fio'] : '';
-        $message = isset($_GET['message']) ? $_GET['message'] : '';
-        $source = isset($_GET['source']) ? $_GET['source'] : '';
-        $file = isset($_GET['file']) ? $_GET['file'] : '';
+    if (isset($_POST['fio'])) {
+        $fio = $_POST['fio'];
+        $file = $_POST['file'];
+        $message = $_POST['message'];
+        $topic = $_POST['topic'];
+        if (isset($_POST['source'])) {
+            $source = $_POST['source'];
+        } else {
+            $source = '';
+        }
+    }
 
-        echo "<h1>Ответ на ваше обращение:</h1>";
-        echo "<p><h2>ФИО:</h2> <span>$fio</span></p>";
-        echo "<p><h2>Текст обращения:</h2> <span>$message</span></p>";
-        echo "<p><h2>Источник:</h2> <span>$source</span></p></h2>";
-        echo "<p><h2>Вы прикрепили файл:</h2><span>$file</span></p></h2>";
-    
-        echo '<form action="header.html">';
-        echo '<input type="submit" value="Заполнить снова">';
-        echo '</form>';
+    echo "<h1>Ответ на ваше обращение:</h1>";
+    echo "<p><h2>ФИО:</h2> <span>$fio</span></p>";
+    echo "<p><h2>Текст обращения:</h2> <span>$message</span></p>";
+    echo "<p><h2>Источник:</h2> <span>$source</span></p></h2>";
+    echo "<p><h2>Вы прикрепили файл:</h2><span>$file</span></p></h2>";
+
+    echo '<br><a class="button" href="index.php?N=' . $_POST['fio'] . '&E=' . $_POST['email'] . '&R='.$source. '">Заполнить снова</a></p></br>';
     ?>
 </body>
-</html>
+
+</html> 
